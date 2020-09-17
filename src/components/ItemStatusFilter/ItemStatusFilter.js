d7fcaad775f58c3c908932ab2fe1238ef189ad85
Artem Veremienko <temaspsxxx@gmail.com>
2020-09-17 17:49:44 +0300

    refactor: change to class component

--- src/components/ItemStatusFilter/ItemStatusFilter.js
+++ src/components/ItemStatusFilter/ItemStatusFilter.js
@@ -1,23 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './ItemStatusFilter.css';
 
-const ItemStatusFilter = () => {
-  return (
-    <div className="btn-group">
-      <button type="button"
-        className="btn btn-info">
-        All
+class ItemStatusFilter extends Component {
+  render() {
+    return (
+      <div className="btn-group">
+        <button type="button"
+          className="btn btn-info">
+          All
       </button>
-      <button type="button"
-        className="btn btn-outline-secondary">
-        Active
+        <button type="button"
+          className="btn btn-outline-secondary">
+          Active
       </button>
-      <button type="button"
-        className="btn btn-outline-secondary">
-        Done
+        <button type="button"
+          className="btn btn-outline-secondary">
+          Done
       </button>
-    </div>
-  );
-};
+      </div>
+    );
+  }
+}
 
 export default ItemStatusFilter;
