f001f507a774e3e1af7591a35e15f112df4b218a
Artem <temaspsxxx@gmail.com>
2020-09-18 00:16:27 +0300

    refactor: change to class component
    Add state for control input.
    Add onSubmit handler for add new todo item.
    Add style margin between input and button.

--- src/components/ItemAddForm/ItemAddForm.js
+++ src/components/ItemAddForm/ItemAddForm.js
@@ -1,16 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './ItemAddForm.css';
 
-const ItemAddForm = ({ onItemAdded }) => {
-  return (
-    <div className="item-add-form">
-      <button
-        className="btn btn-secondary"
-        onClick={() => onItemAdded('text')} >
-        Add Item
-      </button>
-    </div>
-  );
+class ItemAddForm extends Component {
+  state = {
+    label: ''
+  }
+
+  onLabelChange = (e) => {
+    this.setState({
+      label: e.target.value
+    })
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.props.onItemAdded(this.state.label);
+    this.setState({ label: '' })
+  }
+
+  render() {
+    return (
+      <form className="item-add-form d-flex"
+        onSubmit={this.onSubmit} >
+        <input type="text"
+          className="form-control"
+          value={this.state.label}
+          onChange={this.onLabelChange}
+          placeholder="What needs to be done" />
+        <button
+          className="btn btn-outline-secondary"
+          onClick={() => this.props.onItemAdded('text')} >
+          Add Item
+        </button>
+      </form>
+    );
+  }
 }
 
 export default ItemAddForm;
