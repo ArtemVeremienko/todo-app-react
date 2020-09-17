c43d41097df73a335144a3b1e8d51eedfc993c16
Artem Veremienko <temaspsxxx@gmail.com>
2020-09-17 17:13:06 +0300

    refactor: change to class component

--- src/components/TodoListItem/TodoListItem.js
+++ src/components/TodoListItem/TodoListItem.js
@@ -1,31 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './TodoListItem.css';
 
-const TodoListItem = ({ label, important = false }) => {
-  const styles = {
-    color: important ? 'tomato' : 'black',
-    fontWeight: important ? 'bold' : 'normal',
-  };
 
-  return (
-    <span className="todo-list-item">
-      <span
-        className="todo-list-item-label"
-        style={styles}>
-        {label}
+class TodoListItem extends Component {
+  onLabelClick = () => {
+    console.log(`Done: ${this.props.label}`)
+  }
+
+  render() {
+    const { label, important = false } = this.props;
+
+    const styles = {
+      color: important ? 'tomato' : 'black',
+      fontWeight: important ? 'bold' : 'normal',
+    };
+
+    return (
+      <span className="todo-list-item">
+        <span
+          className="todo-list-item-label"
+          style={styles}
+          onClick={this.onLabelClick}>
+          {label}
+        </span>
+
+        <button type="button"
+          className="btn btn-outline-success btn-sm float-right">
+          <span className="fa fa-exclamation" />
+        </button>
+
+        <button type="button"
+          className="btn btn-outline-danger btn-sm float-right">
+          <span className="fa fa-trash-o" />
+        </button>
       </span>
-
-      <button type="button"
-        className="btn btn-outline-success btn-sm float-right">
-        <span className="fa fa-exclamation" />
-      </button>
-
-      <button type="button"
-        className="btn btn-outline-danger btn-sm float-right">
-        <span className="fa fa-trash-o" />
-      </button>
-    </span>
-  );
-};
+    );
+  }
+}
 
 export default TodoListItem;
